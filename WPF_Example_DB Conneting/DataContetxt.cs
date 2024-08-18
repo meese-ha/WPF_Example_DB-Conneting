104000edadf83d1467ae1bc441fc17e9556e9440
tpaks <tpaks@DESKTOP-1NL0OLQ>
2024-08-18 19:45:23 +0900

    초기 커밋.

---
+++ WPF_Example_DB Conneting/DataContetxt.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WPF_Example_DB_Conneting.Models;
+
+namespace WPF_Example_DB_Conneting
+{
+    public class DataContetxt : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source =(localhost); database=TESTDB; User ID=id; Password=pw; Integrated Security=True; Encrypt=False;");
+        }
+
+        public DbSet<TEST_Table> TEST_Table { get; set; }
+    }
+}
---
+++ WPF_Example_DB Conneting/Models/TEST_Table.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_Example_DB_Conneting.Models
+{
+    public class TEST_Table
+    {
+        public string? Id {get; set;}
+
+        public string? Name {get; set;}
+
+        public string? SerialNumber {get; set;}
+
+    }
+}
